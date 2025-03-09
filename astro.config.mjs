af8c339ff2f3d4ecfae3ab4fa84e15c7c97b7cd3
Alejo <alejocstaf@proton.me>
2025-03-09 17:04:06 -0300

    Actualizació v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
+  output: 'static', 
   server: {
     port: 3000,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 3000,
+      clientPort: 3000
+    }
   }
 });
